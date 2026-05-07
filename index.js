77ac9eff7d5d46968e338442b71b0999bf62ea42
AT <alvintan2112@gmail.com>
2026-05-07 13:11:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// 1. define the state and create some mock data
+let tasks = [
+    {
+        "id" : 1,
+        "name": "Wash the car",
+        "dateDue": "2026-05--05",
+        "urgency": 3
+
+    },
+
+    {
+
+        "id" : 2,
+        "name": "Clean the room",
+        "dateDue": "2026-05--31",
+        "urgency": 1
+    },
+
+    {   "id" : 3,
+        "name": "Buy cat food",
+        "dateDue": "2026-05--25",
+        "urgency": 5
+    
+    },
+] 
+
+// task will be an array of objects
+// andeach object must have the following properities: id, name, dueDate and urgency
+function displayTasks(tasks) {
+
+    let i = 0;
+    // tasks.length will giver us how many itmes are in the array
+    while (i < tasks.length) {
+        let currentTask = tasks[i];
+        console.log(`${currentTask.id}. ${currentTask.name} is due on ${currentTask.dateDue} with urgency ${currentTask.urgency} `);
+        // Expected output
+        // "1. Wash the car is due on 2026-05-05 with urgency 3)"
+        i = i + 1;
+    }
+
+        
+}
+
+displayTasks(tasks);
